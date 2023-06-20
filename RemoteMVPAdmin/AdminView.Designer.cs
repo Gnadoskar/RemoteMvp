963196275d01d1e0a2766261b1af1675ddaf80bb
MarkusErtl <markus77ertl@gmail.com>
2023-06-20 13:38:58 +0200

    Dependencies

--- RemoteMVPAdmin/AdminView.Designer.cs
+++ RemoteMVPAdmin/AdminView.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RemoteMVPAdmin
 {
-    partial class Form1
+    partial class AdminView
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // AdminView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "AdminView";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
--- RemoteMVPAdmin/AdminView.cs
+++ RemoteMVPAdmin/AdminView.cs
@@ -1,8 +1,8 @@
 namespace RemoteMVPAdmin
 {
-    public partial class Form1 : Form
+    public partial class AdminView : Form
     {
-        public Form1()
+        public AdminView()
         {
             InitializeComponent();
         }
